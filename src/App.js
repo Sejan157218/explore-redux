487e564fcbb5e2691ce8b3fba259152b7a10fe06
Sejan Mahmud <sejan157218@gmail.com>
2022-02-22 00:11:19 +0600

    create app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Pages/Navbar/Navbar';
+import Shop from './Pages/Shop/Shop';
+
+function App() {
+  return (
+    <div className="">
+      <Navbar></Navbar>
+      <Shop></Shop>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Shop/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div className="text-center mt-5">
+            <h1>Buy Book Price 200 BDT</h1>
+            <button className='btn btn-outline-success mx-2'> + </button>
+            Add to Cart item
+            <button className='btn btn-outline-success mx-2'> - </button>
+        </div>
+    );
+};
+
+export default Shop;
